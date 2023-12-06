c99aa8518ee39bebd39363fdc7df3c55c3e4d492
liujiahao <liujiahao@duxiaoman.com>
2023-12-06 22:57:44 +0800

    fix

--- LJH_test.py
+++ LJH_test.py
@@ -199,6 +199,7 @@ class CrossViT(nn.Module):
         # -------------------------------------------------------------------------
         # Interpolate (or Upsample)
         # -------------------------------------------------------------------------
+        rgb_fea_out = F.interpolate(x, size=([h, w]), mode='bilinear')
         ir_fea_out = F.interpolate(rgb_fea_out, size=([h, w]), mode='bilinear')
         return rgb_fea_out, ir_fea_out
 
