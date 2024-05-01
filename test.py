22997471339f2c75984c02ab88c72949b6e11c9e
liujiahao <liujiahao@duxiaoman.com>
2024-05-01 22:02:34 +0800

    fix test.py

--- test.py
+++ test.py
@@ -298,7 +298,7 @@ def test(data,
 if __name__ == '__main__':
     parser = argparse.ArgumentParser(prog='test.py')
     parser.add_argument('--weights', nargs='+', type=str, default='/home/watanabelab/multispectural-object-detection/liujiahao/runs/train/11.16/weights/best.pt', help='model.pt path(s)')
-    parser.add_argument('--data', type=str, default='./data/multispectral/FLIR_aligned.yaml', help='*.data path')
+    parser.add_argument('--data', type=str, default='./data/multispectral/LLVIP.yaml', help='*.data path')
     parser.add_argument('--batch-size', type=int, default=64, help='size of each image batch')
     parser.add_argument('--img-size', type=int, default=640, help='inference size (pixels)')
     parser.add_argument('--conf-thres', type=float, default=0.001, help='object confidence threshold')
