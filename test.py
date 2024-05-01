1c1f272f4982f0aaa2e321d909431c351bbcf74e
liujiahao <liujiahao@duxiaoman.com>
2024-05-01 23:10:33 +0800

    fix

--- test.py
+++ test.py
@@ -16,8 +16,10 @@ from utils.general import coco80_to_coco91_class, check_dataset, check_file, che
 from utils.metrics import ap_per_class, ConfusionMatrix
 from utils.plots import plot_images, output_to_target, plot_study_txt
 from utils.torch_utils import select_device, time_synchronized
-import sys
-sys.path.append("/multispectral-object-detection/models/common.py")
+import imp
+common = imp.load_source('common', '/home/watanabelab/multispectural-object-detection/liujiahao/multispectral-object-detection/models/common.py')
+activations = common.activations
+
 
 
 def test(data,
